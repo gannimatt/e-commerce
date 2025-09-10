b17ddb7034a9d1c42460f223e957dfd9ed998e1c
Gannimatt <ganimatisayevv@gmail.com>
2025-09-10 22:07:02 +0200

    feat(product): add repository interface

--- src/main/java/com/github/gannimatt/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/github/gannimatt/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.github.gannimatt.ecommerce.repository;
 
 import com.github.gannimatt.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findBySku(String sku);
+    // ProductRepository
+    Page<Product> findByNameContainingIgnoreCaseOrSkuContainingIgnoreCase(String name, String sku, Pageable pageable);
+
 }
