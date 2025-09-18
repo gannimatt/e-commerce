fb14975af3568259113b4c909124354088bb431b
Gannimatt <ganimatisayevv@gmail.com>
2025-09-18 22:18:27 +0200

    chore(boot): tidy ECommerceApplication startup config

--- src/main/java/com/github/gannimatt/ecommerce/ECommerceApplication.java
+++ src/main/java/com/github/gannimatt/ecommerce/ECommerceApplication.java
@@ -2,7 +2,9 @@ package com.github.gannimatt.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.github.gannimatt.ecommerce")
 @SpringBootApplication
 public class ECommerceApplication {
 
